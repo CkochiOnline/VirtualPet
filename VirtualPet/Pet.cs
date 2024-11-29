368e8cb7834d09e6d90f8029f2b0fca5341bce3a
CkochiOnline <ckochionline@gmail.com>
2024-11-29 14:49:57 +0100

    Add project files.

---
+++ VirtualPet/Pet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    public class Pet
+    {
+        public string Name { get; private set; }
+        public int Hunger { get; private set; }
+        public int Hapiness { get; private set; }
+
+        public Pet(string name) // constructor.
+        {
+            Name = name;
+            Hunger = 0;
+            Hapiness = 0;
+        }
+        public void Feed() // feeding pet.
+        {
+            Hunger += 10; // increas hungry level by 10.
+            Console.WriteLine($"{Name} has been fed, hunger level {Hunger}"); // message for food level.
+        }
+        public void Play() // playe with pet.
+        {
+            Hapiness += 10; // increase hapiness level by 10.
+            Console.WriteLine($"{Name} is playing, Hapiness level is {Hapiness}"); // message for food level.
+        }
+        public void CheckStatus() // check status for pet. - Hunger and Hapiness.
+        {
+            Console.WriteLine($"{Name}'s status - Hunger: {Hunger} - Hapiness: {Hapiness}"); // Gives a message.
+        }
+
+    }
+}
---
+++ VirtualPet/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace VirtualPet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is yout pets name?");
+            string petName = Console.ReadLine();
+            Pet myPet = new Pet(petName);
+
+            while (true) 
+            {
+                Console.WriteLine("\nWhat would you like to do?");
+                Console.WriteLine("1. Feed your pet.");
+                Console.WriteLine("2. Play with your pet.");
+                Console.WriteLine("3. Check if your pet is hungry.");
+                Console.WriteLine("4. Exit.");
+                Console.WriteLine("Enter your choice.");
+                string choice = Console.ReadLine();
+
+                switch (choice) 
+                {
+                    case "1":
+                        myPet.Feed();
+                        break;
+                    case "2":
+                        myPet.Play(); 
+                        break;
+                    case "3":
+                        myPet.CheckStatus(); 
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Try again");
+                        break;
+                }
+            }
+        }
+    }
+}
